37476263f759300f106db462a607b8134617eb45
Anthony Shalagin <anthony.shalagin@gmail.com>
2017-06-27 13:31:03 -0400

    Playing around with expect library basic functionality

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,18 +1,15 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
-    var res = utils.add(33,11);
-    
-    if (res != 44) {
-        throw new Error(`Expected 44, but got ${res}.`);
-    }
+    var res = utils.add(33,11);    
+    expect(res).toBe(44).toBeA('number');
+
 });
 
 
 it('should square two numbers', () => {
-    var res = utils.square(5);
-    
-    if (res != 25) {
-        throw new Error(`Expected 25, but got ${res}.`);
-    }
+    var res = utils.square(5);    
+    expect(res).toBe(25).toBeA('number');
 });

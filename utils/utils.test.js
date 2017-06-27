5b6146480a291e22a954173430bc299bad11cbd0
Anthony Shalagin <anthony.shalagin@gmail.com>
2017-06-27 13:43:28 -0400

    Expect with objects

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -4,6 +4,7 @@ const utils = require('./utils');
 
 it('should add two numbers', () => {
     var res = utils.add(33,11);    
+    
     expect(res).toBe(44).toBeA('number');
 
 });
@@ -11,5 +12,24 @@ it('should add two numbers', () => {
 
 it('should square two numbers', () => {
     var res = utils.square(5);    
+    
     expect(res).toBe(25).toBeA('number');
+});
+
+
+it('should expect some values', () => {
+//    expect(12).toNotBe(11);
+//    expect({name: "Anthony"}).toEqual({name: 'Anthony'});
+    expect([2,3,4]).toInclude(2);
+    expect([2,3,4]).toExclude(5);
+    
+    expect({
+        name:'Anthony',
+        age: 21,
+        location: 'Brooklyn'
+    }).toInclude({
+        age:21
+    });
+    
+    
 });

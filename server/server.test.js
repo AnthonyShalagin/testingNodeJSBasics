547d2473c952e851682d83f8e2d9a4c2de268033
Anthony Shalagin <anthony.shalagin@gmail.com>
2017-07-05 21:10:39 -0400

    Another test for an API call

--- server/server.test.js
+++ server/server.test.js
@@ -1,10 +1,28 @@
 const request = require('supertest');
+const expect = require('expect');
 
 var app = require('./server').app;
 
 it('should return hello world response', (done) => {
 	request(app)
 		.get('/')
-		.expect('Hello world!')
-		.end(done);
+		.expect(404	)
+		.expect((res) => {
+			expect(res.body).toInclude({
+				error: 'Page not found.'
+			});
+		})
+		.end(done);	
+});
+
+it('should return names and ages', (done) => {
+	request(app)
+		.get('/users')
+		.expect((res) => {
+			expect(res.body).toInclude({
+				name: 'Anthony', 
+				age: 21
+			});
+		}).
+		end(done);
 });
